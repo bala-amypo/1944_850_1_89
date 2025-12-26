09cfa4eabf9cb8af42b2ec65f62594c50f2b361a
bala-amypo <bala@amypo.in>
2025-12-26 18:17:49 +0000

    User commit

--- src/main/java/com/example/demo/util/DemoApplication.java
+++ src/main/java/com/example/demo/util/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan("com.example.demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
